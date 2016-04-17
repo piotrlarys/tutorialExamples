f0ea41b20de1fde45157288238375e494a55463d
Piot Larys <piotr.larys@nsp.biz.pl>
2016-04-17 20:07:34 +0200

    dialog akcje

--- app/src/main/java/tutorial/larys/info/zad1/dialog/DialogActivity.java
+++ app/src/main/java/tutorial/larys/info/zad1/dialog/DialogActivity.java
@@ -1,22 +1,38 @@
 package tutorial.larys.info.zad1.dialog;
 
 import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import tutorial.larys.info.zad1.R;
 
-public class DialogActivity extends Activity {
+public class DialogActivity extends Activity implements DialogInterface.OnClickListener {
+
+    private TextView selectedOptionText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dialog);
+        selectedOptionText = (TextView) findViewById(R.id.selected_option_text);
     }
 
     public void infoDialog(View view) {
         InfoDialogFragment infoDialogFragment = new InfoDialogFragment();
         infoDialogFragment.show(getFragmentManager(), null);
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+        switch (which) {
+            case DialogInterface.BUTTON_POSITIVE:
+                selectedOptionText.setText("OK");
+                break;
+            case DialogInterface.BUTTON_NEGATIVE:
+                selectedOptionText.setText("Anuluj");
+                break;
+        }
     }
 }
--- app/src/main/java/tutorial/larys/info/zad1/dialog/InfoDialogFragment.java
+++ app/src/main/java/tutorial/larys/info/zad1/dialog/InfoDialogFragment.java
@@ -3,7 +3,9 @@ package tutorial.larys.info.zad1.dialog;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.widget.Toast;
 
 /**
  * Created by Piotr on 2016-04-10.
@@ -15,6 +17,20 @@ public class InfoDialogFragment extends DialogFragment {
         AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
         builder.setTitle("Prosty dialog");
         builder.setMessage("Tutaj treść dialogu");
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getActivity(), "Naciśnięto: OK", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.setNegativeButton("Anuluj", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getActivity(), "Naciśnięto: Anuluj", Toast.LENGTH_SHORT).show();
+            }
+        });
         return builder.create();
     }
 }
